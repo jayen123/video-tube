74896aa7401080d8b46b93c171842fbdcd2abc36
jayen123 <jayenhasan2003@gmail.com>
2024-10-03 02:42:27 +0600

    loaded videos from api and show in display

--- js/video.js
+++ js/video.js
@@ -12,8 +12,68 @@ const loadCategories = async () => {
   }
 };
 
-// displayCategories
+// loadVideos
+const loadVideos = async () => {
+  try {
+    const res = await fetch(
+      "https://openapi.programming-hero.com/api/phero-tube/videos"
+    );
+    const data = await res.json();
 
+    displayVideos(data.videos);
+  } catch (error) {
+    console.log("Error:", error);
+  }
+};
+
+
+// {
+//     "category_id": "1003",
+//     "video_id": "aaaf",
+//     "thumbnail": "https://i.ibb.co/5LRQkKF/stick-and-stones.jpg",
+//     "title": "Sticks & Stones",
+//     "authors": [
+//         {
+//             "profile_picture": "https://i.ibb.co/rdTZrCM/dev.jpg",
+//             "profile_name": "Dave Chappelle",
+//             "verified": true
+//         }
+//     ],
+//     "others": {
+//         "views": "113K",
+//         "posted_date": ""
+//     },
+//     "description": "Dave Chappelle's 'Sticks & Stones' has garnered 113K views and remains a controversial yet highly engaging piece of stand-up comedy. Known for his fearless approach, Dave dives into a wide range of topics, delivering his unique perspective with wit and sharp humor. As a verified artist, Dave's comedy is raw, honest, and unapologetically funny."
+// }
+
+const displayVideos = (videos) => {
+  const videoContainer = document.getElementById("videos");
+  videos.forEach((video) => {
+    console.log(video);
+    
+    const card = document.createElement("div");
+    card.classList = "card card-compact shadow-xl";
+    card.innerHTML = `
+        <figure class="h-[200px]">
+    <img
+      src=${video.thumbnail}
+      alt="Shoes"
+      class="w-full h-full object-cover"
+       />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">${video.title}</h2>
+    <p>${video.description}</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+    `;
+    videoContainer.appendChild(card)
+  });
+};
+
+// displayCategories
 const displayCategories = (categories) => {
   const categoriesContainer = document.getElementById("categories");
   categories.forEach((item) => {
@@ -25,3 +85,4 @@ const displayCategories = (categories) => {
 };
 
 loadCategories();
+loadVideos();
